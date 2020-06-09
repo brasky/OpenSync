e7175eabd12d3a220d1f64e63ee86cee859b0940
Elliot DeMatteis <elliotdematteis@gmail.com>
2020-06-09 00:22:30 -0700

    Renamed app, implemented continous playback of playlist.

--- OpenSync/Client/wwwroot/player.js
+++ OpenSync/Client/wwwroot/player.js
@@ -3,7 +3,6 @@
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 var player;
 function onYouTubeIframeAPIReady(id) {
     console.log("Making player");
@@ -21,9 +20,22 @@ function onPlayerReady(event) {
 }
 
 function onPlayerStateChange(event) {
-    console.log("state change");
-    DotNet.invokeMethodAsync('BlazorApp2.Client', 'jsSync');
-      }
+    console.log("State Change - New Player State: " + currentState);
+    var currentTimestamp = getTimestamp();
+    var duration = getDuration();
+    var currentState = player.getPlayerState()
+    //-1 is "Unstarted" but there is no YT.PlayerState constant for Unstarted.
+    if (currentTimestamp == duration && currentState != -1 && currentState != YT.PlayerState.BUFFERING) {
+        DotNet.invokeMethodAsync('OpenSync.Client', 'jsNextVideo');
+    }
+    else {
+        DotNet.invokeMethodAsync('OpenSync.Client', 'jsSync');
+    }       
+}
+
+function getDuration() {
+    return Math.round(player.getDuration());
+}
 
 function getTimestamp() {
     return Math.round(player.getCurrentTime());
