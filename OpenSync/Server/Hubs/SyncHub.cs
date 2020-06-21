769b27d7e281349c934e0cfece227b9b46c2a051
Elliot DeMatteis <elliotdematteis@gmail.com>
2020-06-21 14:00:50 -0700

    Added leader and automatic leader switching on disconnect.

--- OpenSync/Server/Hubs/SyncHub.cs
+++ OpenSync/Server/Hubs/SyncHub.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace OpenSync.Server.Hubs
 {
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+
+        public static string Leader = string.Empty;
+    }
+
     public class SyncHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            GetLeader();
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            if (UserHandler.Leader == Context.ConnectionId)
+            {
+                if (UserHandler.ConnectedIds.Count > 0)
+                {
+                    UserHandler.Leader = UserHandler.ConnectedIds.First();
+                }
+                else
+                {
+                    UserHandler.Leader = string.Empty;
+                }
+            }
+            GetLeader();
+            return base.OnDisconnectedAsync(exception);
+        }
+
         public async Task SyncVideo(string id, int timestamp, int playlistSize)
         {
+            if (Context.ConnectionId != UserHandler.Leader) return;
             await Clients.Others.SendAsync("ReceiveSync", id, timestamp, playlistSize);
         }
 
@@ -20,6 +53,7 @@ namespace OpenSync.Server.Hubs
 
         public async Task PauseStatus(bool isPaused, int timestamp)
         {
+            if (Context.ConnectionId != UserHandler.Leader) return;
             await Clients.Others.SendAsync("ReceivePauseStatus", isPaused, timestamp);
         }
 
@@ -28,9 +62,20 @@ namespace OpenSync.Server.Hubs
             await Clients.Others.SendAsync("ReceivePlaylist", playlist, id, timestamp);
         }
 
-        public async Task PlaylistRequest(bool _)
+        public async Task PlaylistRequest()
         {
-            await Clients.Others.SendAsync("PlaylistRequest", true);
+            await Clients.Others.SendAsync("PlaylistRequest");
+        }
+
+        public async Task GetLeader()
+        {
+            if (string.IsNullOrEmpty(UserHandler.Leader))
+            {
+                UserHandler.Leader = Context.ConnectionId;
+            }
+
+                await Clients.All.SendAsync("Leader", UserHandler.Leader);
+            
         }
     }
 }
