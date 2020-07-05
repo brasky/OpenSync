889abdf4927d60081a0ea5db310cf75f25ed27fc
Elliot DeMatteis <elliotdematteis@gmail.com>
2020-07-05 01:36:04 -0700

    Rename components with old name

--- OpenSync/Server/Pages/Error.cshtml.cs
+++ OpenSync/Server/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace BlazorApp2.Server.Pages
+namespace OpenSync.Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
